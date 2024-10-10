4c3c5989cbbee2e56d76c7521c820bd10d7c3da8
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2024-10-10 23:37:20 +0300

    chore(backend * bootstrap): cors enabling

--- goods-app-backend/src/main.ts
+++ goods-app-backend/src/main.ts
@@ -3,6 +3,9 @@ import { GoodsModule } from './goods/goods.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GoodsModule);
+  app.enableCors({
+    // origin: 'next_address'
+  })
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
