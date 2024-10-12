c7dceb8827bfd4e2f5ceeb4c4f4bf1b57b995d99
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2024-10-12 03:56:06 +0300

    fix(docker): обновил переменные для пробрасываемых в next адресов бэкенда

---
+++ goods-app-frontend/src/constants/api.ts
@@ -0,0 +1,5 @@
+export const NEST_CSR_API_ADDRESS = `${process.env.NEXT_PUBLIC_NEST_CSR_ADDRESS}/api`;
+
+export const NEST_CSR_PHOTOS_ADDRESS = `${process.env.NEXT_PUBLIC_NEST_CSR_ADDRESS}/photos`;
+
+export const NEST_SSR_API_ADDRESS = `${process.env.NEXT_PUBLIC_NEST_SSR_ADDRESS}/api`;
