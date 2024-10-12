b0ce89c76a95586e8146c533d41abc70d04cadb3
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2024-10-13 00:36:49 +0300

    style(frontend): ReactQueryClientProvider remove div root

--- goods-app-frontend/src/providers/ReactQueryClientProvider.tsx
+++ goods-app-frontend/src/providers/ReactQueryClientProvider.tsx
@@ -7,13 +7,11 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
-export default function ReactQueryClientProvider({children}: { children: React.ReactNode}) {
+export default function ReactQueryClientProvider({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <QueryClientProvider client={queryClient}>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
