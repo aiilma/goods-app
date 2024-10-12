31987e98ff067bc878afc1f93552cf3119fcf792
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2024-10-13 00:46:47 +0300

    fix(frontend * goods * read): выключил Data Cache для получения товара

--- goods-app-frontend/src/api/server-api.ts
+++ goods-app-frontend/src/api/server-api.ts
@@ -1,7 +1,9 @@
-import { GoodResponse } from '@/types/goods';
 import { NEST_SSR_API_ADDRESS } from '@/constants/api';
+import { Good } from '@/types/goods';
 
-export const fetchGood = async (goodId: string): Promise<GoodResponse> => {
-  const response = await fetch(`${NEST_SSR_API_ADDRESS}/goods/${goodId}`);
-  return response.json();
+export const fetchGood = async (goodId: string): Promise<Good> => {
+  const response = await fetch(`${NEST_SSR_API_ADDRESS}/goods/${goodId}`, {
+    cache: 'no-store',
+  });
+  return await response.json();
 };
