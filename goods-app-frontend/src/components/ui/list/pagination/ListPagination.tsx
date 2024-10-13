5ee180c2d24c9cfae7a23cb903a21633436f0bd8
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2024-10-13 05:48:55 +0300

    feat(frontend * components * ui): +ListPagination

---
+++ goods-app-frontend/src/components/ui/list/pagination/ListPagination.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { Pagination } from 'antd';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+const DEFAULT_PAGE = 1
+const DEFAULT_LIMIT = 10
+
+type ListPaginationProps = {
+  total: number
+  limit?: number
+  defaultCurrent?: number
+}
+
+export default function ListPagination(props: ListPaginationProps) {
+  const { total, limit, defaultCurrent } = props;
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const currentPage = defaultCurrent || DEFAULT_PAGE
+  const pageSize = limit || DEFAULT_LIMIT
+  const showTotal = (total) => `Всего ${total} товаров`;
+
+  let isPageSizeChanged = false;
+
+  const handlePageSizeChange = (current, size) => {
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.set('limit', size.toString());
+
+    const newCurrentPage = Math.min(current, Math.ceil(total / size));
+    if (newCurrentPage > 1) {
+      newSearchParams.set('page', newCurrentPage.toString());
+    } else {
+      newSearchParams.delete('page');
+    }
+
+    isPageSizeChanged = true;
+
+    router.push(`${pathname}?${newSearchParams.toString()}`);
+  };
+
+  const handlePageChange = (page) => {
+    if (isPageSizeChanged) {
+      isPageSizeChanged = false;
+      return;
+    }
+
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+
+    if (page > 1) {
+      newSearchParams.set('page', page.toString());
+    } else {
+      newSearchParams.delete('page');
+    }
+
+    router.push(`${pathname}?${newSearchParams.toString()}`);
+  };
+
+  return (
+    <Pagination
+      total={total}
+      showTotal={showTotal}
+      defaultPageSize={pageSize}
+      defaultCurrent={currentPage}
+      onChange={handlePageChange}
+      onShowSizeChange={handlePageSizeChange}
+    />
+  );
+}
