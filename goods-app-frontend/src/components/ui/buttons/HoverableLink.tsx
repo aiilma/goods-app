a20f720dfdde8c6c15c0863dd3940504aa194506
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2024-10-13 20:26:59 +0300

    feat(frontend * components * ui): для HoverableLink добавлен spinner

--- goods-app-frontend/src/components/ui/buttons/HoverableLink.tsx
+++ goods-app-frontend/src/components/ui/buttons/HoverableLink.tsx
@@ -1,11 +1,33 @@
-"use client";
+'use client';
 
 import Link from 'next/link';
 import styles from './styles.module.css';
+import { useRouter } from 'next/navigation';
+import { useTransition } from 'react';
+import { Spin } from 'antd';
 
 function HoverableLink({ children, href }) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  if (isPending) {
+    return (
+      <Spin tip="Loading..." className={styles.spinner} />
+    );
+  }
+
   return (
-    <Link href={href} className={styles.hoverableLink}>
+    <Link
+      href={href}
+      className={styles.hoverableLink}
+      onClick={(e) => {
+        e.preventDefault();
+        startTransition(() => {
+          const url = href.toString();
+          router.push(url);
+        });
+      }}
+    >
       {children}
     </Link>
   );
