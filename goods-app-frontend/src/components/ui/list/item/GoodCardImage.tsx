f727f5894dfcd93aea214137050ca4d72867a1aa
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2024-10-13 09:37:12 +0300

    feat(frontend * goods * list): обработка кейсов отсутствия изображения локализованная на уровне компонента

--- goods-app-frontend/src/components/ui/list/item/GoodCardImage.tsx
+++ goods-app-frontend/src/components/ui/list/item/GoodCardImage.tsx
@@ -2,19 +2,38 @@
 
 import { Image } from 'antd';
 import { NEST_CSR_PHOTOS_ADDRESS } from '@/constants/api';
+import { useState } from 'react';
 
 export default function GoodCardImage({ src, alt, ...props }) {
+  const [isError, setIsError] = useState(false);
+
+  const handleError = () => {
+    setIsError(true);
+  };
+
   return (
     <div style={{
       minHeight: '150px',
     }}>
-      {src && (
+      {src && !isError? (
         <Image
           src={`${NEST_CSR_PHOTOS_ADDRESS}/${src}`}
           alt={alt}
           style={{ width: '100%', height: '150px', objectFit: 'cover' }}
+          onError={handleError}
           {...props}
         />
+      ) : (
+        <div style={{
+          width: '100%',
+          height: '150px',
+          backgroundColor: '#f0f0f0',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          Нет изображения
+        </div>
       )}
     </div>
   );
