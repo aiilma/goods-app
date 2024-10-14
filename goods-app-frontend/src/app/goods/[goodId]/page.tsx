3c5e6567d9e50ea5ed9039ac0ae46cf5d1362921
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2024-10-14 07:58:08 +0300

    feat(frontend * OpenGraph): метатеги для товара. надо тестить

--- goods-app-frontend/src/app/goods/[goodId]/page.tsx
+++ goods-app-frontend/src/app/goods/[goodId]/page.tsx
@@ -4,6 +4,8 @@ import Paragraph from 'antd/lib/typography/Paragraph';
 import Card from 'antd/lib/card/Card';
 import GoodActions from '@/components/GoodActions';
 import GoodCardImage from '@/components/ui/list/item/GoodCardImage';
+import Head from 'next/head';
+import { NEST_CSR_PHOTOS_ADDRESS } from '@/constants/api';
 
 export default async function GoodPage({ params }: {
   params: {
@@ -18,6 +20,15 @@ export default async function GoodPage({ params }: {
       width: '50%',
       textAlign: 'center',
     }}>
+      <Head>
+        <title>{good.name}</title>
+        <meta name="description" content={good.description} />
+        <meta property="og:title" content={good.name} />
+        <meta property="og:description" content={good.description} />
+        <meta property="og:image" content={`${NEST_CSR_PHOTOS_ADDRESS}/${good.photo}`} />
+        <meta property="og:type" content="product" />
+      </Head>
+
       <GoodActions goodId={good.id} />
       {good.photo && (
         <GoodCardImage
